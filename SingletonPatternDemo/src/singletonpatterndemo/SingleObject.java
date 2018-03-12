44143ac06668ef0a1aa75e08754bb6bb026a57ca
Wai Yan Phyoe <waiyanphyoe@elissa.software>
2018-03-12 12:18:43 +0630

    Initial Commit
    Initial Commit

---
+++ SingletonPatternDemo/src/singletonpatterndemo/SingleObject.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package singletonpatterndemo;
+
+/**
+ *
+ * @author elissa
+ */
+public class SingleObject {
+
+    //Create an object of SingleObject
+    private static SingleObject instance = new SingleObject();
+
+    //Make the constructor private so that this class cannot be
+    //Instantiated
+    private SingleObject() {
+    }
+
+    //Get the only object available
+    public static SingleObject getInstance() {
+        return instance;
+    }
+
+    public void showMessage() {
+        System.out.println("Hello World!");
+    }
+}
